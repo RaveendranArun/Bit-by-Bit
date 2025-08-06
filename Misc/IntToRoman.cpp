45743d1d14c662b90ba6ad093ea8703637d1a9ce
Arun Raveendran <arunr4957@gmail.com>
2025-08-06 09:36:11 +0530

    Integer to Roman

--- Misc/IntToRoman.cpp
+++ Misc/IntToRoman.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <map>
+#include <vector>
 
 using namespace std;
 
 string int_to_roman(int n)
 {
     
-    int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4,1};
-    string sym[]= {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    vector<int> val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4,1};
+    vector<string> sym= {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     string ans;
     int i = 0;
